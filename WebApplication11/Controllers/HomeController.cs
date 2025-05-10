95b7cea8a125801bedef088ca980dbf9e0caddc6
Misha <misha.gefkm14@gmail.com>
2025-05-10 17:41:49 +0300

    regest

---
+++ WebApplication11/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication11;
+
+namespace WebApplication11.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationContext _context;
+
+        public HomeController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Users.Add(user);
+                _context.SaveChanges();
+                return RedirectToAction("Success");
+            }
+            return View(user);
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication11/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication11
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Введите имя")]
+
+        public string Name { get; set; } = ""; // имя пользователя
+        [Range(1, 120, ErrorMessage = "Возраст должен быть от 1 до 120")]
+
+        public int Age { get; set; }
+    }
+}
